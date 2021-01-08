be5d41971d1f699eb7848cc250e01754634d72b6
Baptiste <bat.boleat13@gmail.com>
2021-01-08 14:56:56 +0100

    fix synchronization problem to get User data

--- src/main/webapp/app/homeInstructor/homeInstructor.component.ts
+++ src/main/webapp/app/homeInstructor/homeInstructor.component.ts
@@ -5,6 +5,7 @@ import { LocalStorageService } from 'ngx-webstorage';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 import { InstructorService } from 'app/entities/instructor/instructor.service';
+import { UserService } from 'app/core/user/user.service';
 
 import { Instructor } from 'app/shared/model/instructor.model';
 import { User } from 'app/core/user/user.model';
@@ -25,6 +26,7 @@ export class HomeInstructorComponent implements OnInit, OnDestroy {
 
   constructor(
     private accountService: AccountService,
+    private userService: UserService,
     private localStorage: LocalStorageService,
     private instructorService: InstructorService
   ) {}
@@ -47,16 +49,44 @@ export class HomeInstructorComponent implements OnInit, OnDestroy {
     this.account = account;
     if (this.accountService.hasAnyAuthority('ROLE_ADMIN')) {
       this.user = this.getCurrentUser();
+      if (this.user == null) {
+        this.getCurrentUserAsynchronously();
+      }
     } else {
       this.instructor = this.getCurrentUser();
+      if (this.instructor == null) {
+        this.getCurrentInstructorAsynchronously();
+      }
     }
   }
 
   getCurrentUser(): Instructor | User | null {
     this.raw = localStorage.getItem('currentUser');
+    // eslint-disable-next-line no-console
+    console.log(this.raw);
     if (this.raw != null) {
       return JSON.parse(this.raw);
     }
     return null;
+  }
+
+  getCurrentUserAsynchronously(): void {
+    if (this.account != null) {
+      this.userService.find(this.account.login).subscribe(user => {
+        this.user = user;
+      });
+    }
+  }
+
+  getCurrentInstructorAsynchronously(): void {
+    if (this.account != null) {
+      this.userService.find(this.account.login).subscribe(user => {
+        this.instructorService.findbyuser(user.id).subscribe(instructor => {
+          // eslint-disable-next-line no-console
+          console.log(instructor);
+          this.instructor = instructor.body;
+        });
+      });
+    }
   }
 }
