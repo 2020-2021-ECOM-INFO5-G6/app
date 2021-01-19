bace674056368d5a148a17c7136985b593f14911
SCourthial <samuel.courthial.18@gmail.com>
2021-01-19 10:04:24 +0100

    Can get all students information
    Added a method for that

--- src/main/webapp/app/homeInstructor/homeInstructor.component.ts
+++ src/main/webapp/app/homeInstructor/homeInstructor.component.ts
@@ -7,6 +7,7 @@ import { Account } from 'app/core/user/account.model';
 import { InstructorService } from 'app/entities/instructor/instructor.service';
 import { UserService } from 'app/core/user/user.service';
 import { ActivityService } from 'app/entities/activity/activity.service';
+import { StudentService } from 'app/entities/student/student.service';
 
 import { Instructor } from 'app/shared/model/instructor.model';
 import { User } from 'app/core/user/user.model';
@@ -32,7 +33,8 @@ export class HomeInstructorComponent implements OnInit, OnDestroy {
     private userService: UserService,
     private localStorage: LocalStorageService,
     private instructorService: InstructorService,
-    private activityService: ActivityService
+    private activityService: ActivityService,
+    private studentService: StudentService
   ) {}
 
   ngOnInit(): void {
@@ -92,6 +94,13 @@ export class HomeInstructorComponent implements OnInit, OnDestroy {
 
   downloadContent(id: number): void {
     this.activityService.download(id).subscribe(string => {
+      const blob = new Blob([string], { type: 'text/plain;charset=utf-8' });
+      saveAs(blob);
+    });
+  }
+
+  downloadAllContent(): void {
+    this.studentService.download().subscribe(string => {
       const blob = new Blob([string], { type: 'text/plain;charset=utf-8' });
       saveAs(blob);
     });
